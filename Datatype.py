8c8930ef7fa8df0bc287590df054b2fbe9ea293f
mohassan22 <77343403+mohassan22@users.noreply.github.com>
2021-09-03 18:08:15 +0200

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,15 @@
+#Welcome messagr
+print("Welcome!")
+
+age  = 18
+#Ask the user for your name
+name = input("Please,Enter your name? ")
+print("We're glad you're here ",name)
+
+Nots = input("What do you think of our website?")
+
+if Nots =="good":
+    print("Thank U")
+
+else:
+    print("Tanks , we will try to fix that")
