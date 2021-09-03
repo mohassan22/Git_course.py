9c77320ad2ec52a0014e5e3eebb1cffb397e393e
mohassan22 <77343403+mohassan22@users.noreply.github.com>
2021-09-03 15:23:44 +0200

    Add files via upload

---
+++ 01_03.py
@@ -0,0 +1,12 @@
+#This is massage for challenge.
+print("Challenge 1: ")
+
+#massage for the user!
+message=input("pleas,Enter your message!")
+Message="Hello"
+print(message)
+
+result=2**3
+print("2**3=",result)
+#print("5-3=",result)
+print("challenge complete!")
