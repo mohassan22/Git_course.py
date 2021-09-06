c0ccfa77dbeae0717de6ab66f75d0331d2dfb810
mohassan22 <77343403+mohassan22@users.noreply.github.com>
2021-09-06 19:35:53 +0200

    Add files via upload

---
+++ function.py
@@ -0,0 +1,9 @@
+#Creat function 
+def favorite_city(name):
+    
+    print("One of my favorite cities is ",name)
+
+#call function
+favorite_city("Cairo,Alex")
+favorite_city("Los Angeles")
+favorite_city("London")
